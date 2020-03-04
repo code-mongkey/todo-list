0496ded244904383333e29c9f85405895fc6e835
haeng <ymca0147@naver.com>
2020-03-05 00:22:54 +0900

    Updates

---
+++ precache-manifest.9e1f2b9b8d4c6100f59d9bd64baaa602.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b75421ae15e149b1e7fc5f8c909e914c",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "d10e4df6d0eb0724fb36",
+    "url": "/todo-list/static/css/main.95990b67.chunk.css"
+  },
+  {
+    "revision": "42abd84ff884045f6ebd",
+    "url": "/todo-list/static/js/2.5e76d5a7.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/todo-list/static/js/2.5e76d5a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d10e4df6d0eb0724fb36",
+    "url": "/todo-list/static/js/main.1c61dfbd.chunk.js"
+  },
+  {
+    "revision": "492b6893fe135a4273bc",
+    "url": "/todo-list/static/js/runtime-main.7e9ae0c4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todo-list/static/media/logo.5d5d9eef.svg"
+  }
+]);
